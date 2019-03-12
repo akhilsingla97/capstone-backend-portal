ac34ded48826c209a65e29ddf0c3fcbc59a19c62
Pritish Thakkar <prince.thakkar001@gmail.com>
2019-03-12 22:28:42 +0530

    added UI orders page

--- index.js
+++ index.js
@@ -1,56 +1,115 @@
 //Running app on port 3000
-const express = require('express');
-const bodyParser = require('body-parser');
-const firebase = require('firebase');
+const express = require("express");
+const bodyParser = require("body-parser");
+const firebase = require("firebase");
 
 const app = express();
 
 //Local files can be read from this folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //using body-parsers to read post API data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false})); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "pug");
 
 //Setting up server
-const server = app.listen(process.env.PORT || 3000, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+const server = app.listen(process.env.PORT || 3000, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
 
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
+app.get("/", function(req, res) {
+  res.render(__dirname + "/orderlist", {
+    data: [
+      {
+        id: "2113131212131232",
+        item: "Pizza",
+        quantity: 3,
+        status: "placed",
+        table: 322,
+        info: "None"
+      },
+      {
+        id: "211323de212131232",
+        item: "Pasta",
+        quantity: 2,
+        status: "placed",
+        table: 322,
+        info: "None"
+      },
+      {
+        id: "211wewr12131232",
+        item: "Tomato Soup",
+        quantity: 3,
+        status: "placed",
+        table: 322,
+        info: "None"
+      },
+      {
+        id: "2113131212131232",
+        item: "Pizza",
+        quantity: 3,
+        status: "placed",
+        table: 322,
+        info: "None"
+      },
+      {
+        id: "2113131212131232",
+        item: "Pizza",
+        quantity: 3,
+        status: "placed",
+        table: 322,
+        info: "None"
+      },
+      {
+        id: "2113131212131232",
+        item: "Pizza",
+        quantity: 3,
+        status: "placed",
+        table: 322,
+        info: "None"
+      },
+      {
+        id: "2113131212131232",
+        item: "Pizza",
+        quantity: 3,
+        status: "placed",
+        table: 322,
+        info: "None"
+      }
+    ]
+  });
+  // res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
 });
 
 // Initialize Firebase
-var config = require("./config.json")
+var config = require("./config.json");
 
 firebase.initializeApp(config);
 
-console.log(firebase)
+console.log(firebase);
 
-var database = firebase.database()
-var ref = database.ref('test')
+var database = firebase.database();
+var ref = database.ref("test");
 var sampledata = {
-	name: "Akhil",
-	order: "Noodles"
+  name: "Akhil",
+  order: "Noodles"
+};
+
+ref.push(sampledata);
+
+ref.on("value", gotData, errData);
+
+function gotData(data) {
+  console.log(data.val());
 }
 
-ref.push(sampledata)
-
-ref.on('value', gotData, errData)
-
-function gotData(data){
-	console.log(data.val())
+function errData(err) {
+  console.log("error");
+  console.log(err);
 }
 
-function errData(err){
-	console.log('error');
-	console.log(err);
-}
-
-console.log(database)
+console.log(database);
 //var ref = database.ref('');
-
-
