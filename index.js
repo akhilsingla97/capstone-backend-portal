532227fee408d3a0f298a23ba6e62175365faebd
akhilsingla97 <akhilsingla97@gmail.com>
2019-04-13 15:01:32 +0530

    New features added- waiter requests, dataformat correction, database updation

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const firebase = require('firebase');
-const path = require('path');
 
 const app = express();
 
@@ -27,25 +26,20 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var ref = database.ref('capstone-f942f')
 
-const testdata = require('./sampletest.json')
-
-//Home page- rendering using sample data as of now
-// app.get("/", (req, res) => {
-//     // console.log(testData)
-//     res.render(__dirname + "/index", testdata);
-// });
-
+//variables to be used in all functions and files
 var sendData = {};
-var testData = [];
+var foodData = [];
+var waiterData = [];
 
 app.get("/", (req, res, next) => {
     sendData = {};
-    testData = [];
+    foodData = [];
+    waiterData = [];
     ref = database.ref('TableNo101')
+    //This data contains both- food and waiter data
     ref.on('value', (data)=> {
-        console.log(data.val());
-        // var testData = []
-        var orderId = []
+        //data for food orders
+        var orderId = [];
         for(item in data.val().Order){
             orderId.push(item);
         }
@@ -54,7 +48,8 @@ app.get("/", (req, res, next) => {
             var items = []
             var tmp = {
                     "id" : orderId[i],
-                    "status" : "Pending",
+                    // "status" : "Pending",
+                    "status" : data.val().Order[orderId[i]].status,
                     "table" : "101",
                     "info" : data.val().Order[orderId[i]].special_instruction
             }
@@ -66,46 +61,47 @@ app.get("/", (req, res, next) => {
                 });
                 tmp["items"] = items;
             }
-            testData.push(tmp);
+            foodData.push(tmp);
         }
-        sendData["data"] = testData;
-        // console.log(orderId);
-        console.log(sendData);
+        sendData["data"] = foodData;
+
+        //data for waiterwork
+        var waiterId = [];
+        for(item in data.val().waiterWork){
+            waiterId.push(item);
+        }
+        for(var i=0;i<waiterId.length;i++){
+            if(data.val().waiterWork[waiterId[i]].waiter == 'required'){
+                waiterData.push({
+                    "id" : waiterId[i],
+                    "waiter" : "required",
+                    "table" : "101",
+                    "purpose" : data.val().waiterWork[waiterId[i]].purpose
+                });
+            }
+        }
+        sendData["waiterWork"] = waiterData;
+        console.log("sendData: ",sendData);
     }, (err)=>{
         console.log(err)
     })
     next()
     }, (req, res)=>{
-        // console.log()
         res.render(__dirname + "/index", sendData);
 });
 
 
-//triggered on confirmation of order
-app.post('/confirm/:orderId', (req,res) => {
-    let orderId = req.params.orderId;
-    let ref = database.ref('test/'+orderId)
-    ref.on('value', (data) => {
-        //let orderId = req.params.orderId;
-        console.log(orderId);
-        console.log(data.val());
-        res.sendStatus(200);
-    }, (err) => {
-        console.log(err);
-        res.sendStatus(500);
-    })
-});
-
 //triggered when order is prepared
 app.post('/prepared/:orderId', (req,res) => {
     let orderId = req.params.orderId;
-    ref = database.ref('test/orderId')
-    ref.on('value', (data) => {
-        //Add the suitable query here
-        console.log(orderId);
-        res.sendStatus(200);
-    }, (err) => {
-        console.log(err);
-        res.sendStatus(500);
-    })
+    ref = database.ref('TableNo101/Order/'+orderId);
+    ref.update({status:"Prepared"});
+    res.sendStatus(200);
+});
+
+app.post('/sendWaiter/:requestId', (req,res) =>{
+    let requestId = req.params.requestId;
+    ref = database.ref('/TableNo101/waiterWork/'+requestId);
+    ref.update({waiter:"catered"});
+    res.sendStatus(200);
 });
--- public/script.js
+++ public/script.js
@@ -43,3 +43,15 @@ function decrementCounter(itemid, orderid) {
   }
   document.getElementById(buttonId).disabled = true;
 }
+
+function SendingWaiter(id) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      $("#" + id).remove();
+      alert("Waiter sent...");
+    }
+  };
+  xhttp.open("POST", "sendWaiter/" + id, true);
+  xhttp.send();
+}
